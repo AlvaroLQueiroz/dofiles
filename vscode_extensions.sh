9fe7366ff72c17742c254fd4afb2e265064d720a
Alvaro Leonel <alvaro.lqueiroz@gmail.com>
2018-11-06 17:10:31 -0300

    Dump VSCode extensions

---
+++ vscode_extensions.sh
@@ -0,0 +1,7 @@
+code --install-extension bibhasdn.django-html
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension formulahendry.auto-close-tag
+code --install-extension mkaufman.HTMLHint
+code --install-extension ms-python.python
+code --install-extension PeterJausovec.vscode-docker
