da93ff76e9536498f9eb3ecc1ea536db3cdbdefc
Alvaro Mariano <alvaro.lqueiroz@gmail.com>
2024-06-28 10:06:40 -0300

    Update everything

--- .zshrc
+++ .zshrc
@@ -1,26 +1,14 @@
+###### VARIABLES ######
+export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
+export PATH="$PATH:/Users/alvaro/.local/bin"
+export STARSHIP_CONFIG="/Users/alvaromariano/.starship/config.toml"
+export STARSHIP_CACHE="~/.starship/cache"
 
-alias docker-clean='docker stop $(docker ps -aq);docker rm $(docker ps -aq)'
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-# TODO:
-# 1 - Alias para criar automaticamente venv para projeto
-# 2 - Alias para atualizar dependencias python
-# 3 - Alias para atualizar dependencias node
-# 4 - Alias para atualizar dependencias do vscode no repo
-# 5 - Alias para atualizar configs(git, vscode, bash, etc) no repo (jq para juntar json)
-
-
-# create a virtualenv with
-# pyenv virtualenv <py_version> <project_name>
-# go to project directory and run
-# pyenv local <project_name>
-
-# Path to your oh-my-zsh installation.
+##### ZSH #########
 export ZSH="$HOME/.oh-my-zsh"
-
 ZSH_THEME="robbyrussell"
 ZSH_DISABLE_COMPFIX=true
-
 plugins=(
     # git
     # pyenv
@@ -29,28 +17,19 @@ plugins=(
     poetry
 )
 
-source $ZSH/oh-my-zsh.sh
 
-
-
-# Set personal aliases, overriding those provided by oh-my-zsh libs,
-# plugins, and themes. Aliases can be placed here, though oh-my-zsh
-# users are encouraged to define aliases within the ZSH_CUSTOM folder.
-# For a full list of active aliases, run `alias`.
+######### ALIASES ########
 alias zshconfig="code ~/.zshrc"
 alias ohmyzsh="code ~/.oh-my-zsh"
 alias docker-clean='docker stop $(docker ps -aq);docker rm $(docker ps -aq)'
-
 function frm() {
   find `$1` -name $2 -exec rm -rf {} \;
 }
 
 
-export LDFLAGS="-L/usr/local/opt/openssl@1.1/lib"
-export CPPFLAGS="-I/usr/local/opt/openssl@1.1/include"
-export PATH="$PATH:/Users/alvaro/.local/bin"
-source "$HOME/.cargo/env"
-
+##### LOAD #######
+source $ZSH/oh-my-zsh.sh
+eval "$(/opt/homebrew/bin/brew shellenv)"
 eval "$(starship init zsh)"
 eval "$(pyenv init --path)"
 eval "$(pyenv init --path -)"
--- configure-enviroment.sh
+++ configure-enviroment.sh
@@ -1,84 +1,32 @@
-#!/usr/bin/env bash
-
-
 # TODOS
 # 1- Verificar e instalar zlib
 
-if ! [ -x "$(command -v npm)" ]; then
-  echo 'Node is not installed!'
-  echo 'Installing node...'
-  brew install node
-  curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-  sudo apt-get install -y nodejs
-  echo 'Installed!'
-fi
+/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"    
+/bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-if ! [ -x "$(command -v git)" ]; then
-  echo 'Git is not installed!'
-  echo 'Installing git...'
-  sudo apt install -y git
-  echo 'Installed!'
-fi
+brew install xz
+brew install node@22
+brew install git
+brew install pyenv
+brew install --cask visual-studio-code
+brew install pipx
+brew install pyenv-virtualenv
+brew install --cask font-fira-code
+brew intall poetry
+brew install starship
+rew install logi-options-plus  
+rew install spotify
+brew install --cask docker
 
-if ! [ -x "$(command -v pyenv)" ]; then
-  echo 'pyenv is not installed!'
-  echo 'Installing pyenv...'
-  curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
-  exec bash
-  pyenv update
-  echo 'Installed!'
-fi
+pyenv install 3.10
+pyenv install 3.11
+pyenv install 3.12
+pyenv global 3.12 3.11 3.10 system
 
-if ! [ -x "$(command -v jq)" ]; then
-  echo 'jq is not installed!'
-  echo 'Installing pyenv...'
-  sudo apt-get install jq
-  echo 'Installed!'
-fi
-
-if ! [ -x "$(command -v code)" ]; then
-  echo 'VSCode is not installed!'
-  echo 'Installing VSCode...'
-  # TODO
-  echo 'Installed!'
-fi
-
-# Instal VSCode extensions
-curl -L https://github.com/AlvaroLQueiroz/dofiles/raw/master/vscode_extensions.sh | bash
-
-# Configure pyenv enviroments
-pyenv install 2.7.15
-pyenv install 3.6.6
-
-pyenv virtualenv 2.7.15 ipython2
-pyenv virtualenv 3.6.6 ipython3
-
-pyenv activate ipython2
-pip install ipython -y
-pyenv deactivate
-
-pyenv activate ipython3
-pip install ipython -y
-pyenv deactivate
-
-pyenv global ipython2 ipython3 3.6.6 2.7.15 system
-
-# Configure git
-# TODO: Perguntar se deseja configurar user name do git ou usar o do repo.
-curl -L https://github.com/AlvaroLQueiroz/dofiles/raw/master/.gitconfig >> $HOME/.gitconfig
-
-# Configure VSCode settings
-#TODO: Suportar diferentes SO's
-curl -L https://github.com/AlvaroLQueiroz/dofiles/raw/master/vscode.json > $HOME/.config/Code/User/settings.json
-# MacOS location
-# curl -L https://github.com/AlvaroLQueiroz/dofiles/raw/master/vscode.json > $HOME/Library/Application\ Support/Code/User/settings.json
+pipx ensurepath
+sudo pipx ensurepath --global # optional to allow pipx actions with --global argument
+pipx install virtualenv
 
 # Increases number of files watched by VSCode
 echo "fs.inotify.max_user_watches=524288" >> /etc/sysctl.conf
 sysctl -p
-
-# Configure Terminal settings
-curl -L https://github.com/AlvaroLQueiroz/dofiles/raw/master/.bashrc >> $HOME/.bashrc
-
-# TODO
-#curl -XGET https://raw.githubusercontent.com/docker/cli/master/contrib/completion/bash/docker > $(brew --prefix)/etc/bash_completion.d/docker
